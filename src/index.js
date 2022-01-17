6ffe95849a811dad7b104f36bab7352d3a6a451f
anurag-felix <anurag.c@felixhealthcare.ai>
2022-01-18 00:31:28 +0530

    reorder corrected 1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import "rsuite/lib/styles/index.less"; // or 'rsuite/dist/styles/rsuite-default.css'
 
 ReactDOM.render(
   <React.StrictMode>
